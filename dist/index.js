9929c1e496b32b63196568a352d596bd8844b34b
almeizaarvin <13519066@std.stei.itb.ac.id>
2024-02-25 00:31:57 +0700

    Add params

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
-    res.send('Ini get!');
+    res.send({
+        message: 'Hai',
+        data: req.params.id
+    });
 });
 app.post('/', (req, res) => {
     res.send({
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-
+import { Request, Response } from 'express';
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Ini get!');
-})
+app.get('/:id', (req: Request, res: Response) => {
+    res.send({
+        message: 'Hai',
+        data: req.params.id
+    })})
 
-app.post('/',(req, res)=>{
+app.post('/',(req: Request, res: Response)=>{
     res.send({
         data: req.body
-    });
+    })
 })
 
 app.listen(3000, () => {
