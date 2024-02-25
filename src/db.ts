e01b748c13239fc4825d1f75a2f57402f464db36
almeizaarvin <13519066@std.stei.itb.ac.id>
2024-02-25 19:08:22 +0700

    Modify env

--- src/db.ts
+++ src/db.ts
@@ -2,10 +2,10 @@ import mysql from 'mysql';
 
 // Create a MySQL connection pool
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'your_username',
-  password: 'your_password',
-  database: 'your_database_name'
+  host: '127.0.0.1',
+  user: 'root@localhost',
+  password: '',
+  database: 'blog_db'
 });
 
 export default pool;
