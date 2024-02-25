dbe85a15a1fea30c0856c70966fccc8235c5512c
almeizaarvin <13519066@std.stei.itb.ac.id>
2024-02-25 22:03:34 +0700

    Add dotenv

--- dist/db.js
+++ dist/db.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql_1 = __importDefault(require("mysql"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// Load environment variables from .env file
+dotenv_1.default.config();
+console.log('Nilai DB_HOST:', process.env.DB_HOST);
+console.log('Nilai DB_USER:', process.env.DB_USER);
 // Create a MySQL connection pool
 const pool = mysql_1.default.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'password',
-    database: 'blog_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 exports.default = pool;
--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,13 @@
 import mysql from 'mysql';
+import dotenv from 'dotenv';
 
-// Create a MySQL connection pool
+dotenv.config();
+
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'password',
-  database: 'blog_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 export default pool;
